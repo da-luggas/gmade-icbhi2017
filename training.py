fb2e0741c481a40e848497fd3dfdc8be975b96e9
Lukas Selch <selchlukas@icloud.com>
2023-10-19 09:15:12 +0200

    added validation, fixed snippet dim

--- training.py
+++ training.py
@@ -5,6 +5,8 @@ from torch.utils.data import DataLoader, random_split, Subset
 
 import random
 from tqdm import tqdm
+from sklearn.metrics import roc_auc_score, roc_curve
+import matplotlib.pyplot as plt
 
 from model import GMADE
 from dataset import RespiratorySoundDataset
@@ -15,12 +17,15 @@ random.seed(seed)
 torch.manual_seed(seed)
 
 # Negative Log Likelihood (NLL) loss for the Gaussian distribution:
-def nll_gaussian(y, mu, logvar):
+def nll_gaussian(y, mu, logvar, eval=False):
     # Calculate the variance from logvar
     var = torch.exp(logvar)
 
     # Calculate the Gaussian NLL
-    nll = (0.5 * (logvar + ((y - mu) ** 2) / var + torch.log(torch.tensor(2.0 * 3.141592653589793)))).sum()
+    if eval is True:
+        nll = (0.5 * (logvar + ((y - mu) ** 2) / var + torch.log(torch.tensor(2.0 * 3.141592653589793)))).sum(1)
+    else:
+        nll = (0.5 * (logvar + ((y - mu) ** 2) / var + torch.log(torch.tensor(2.0 * 3.141592653589793)))).sum()
 
     return nll
 
@@ -28,13 +33,15 @@ def nll_gaussian(y, mu, logvar):
 ## CONFIGURATION ##
 ###################
 
-epochs = 100
+epochs = 1
 lr = 3e-4
 batch_size = 128
 hidden_size = 100
+patience = 10
 
 device = torch.device('mps') if torch.backends.mps.is_available() else (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
 device = torch.device('cpu') # REMOVE: debugging only
+
 ##############
 ## TRAINING ##
 ##############
@@ -67,13 +74,20 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 model = GMADE(hidden_size=hidden_size, device=device)
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
+# Initialize best validation loss and patience counter for early stopping
+best_val_loss = float('inf')
+wait = 0
+
 # Train the model
 for epoch in tqdm(range(epochs)):
     total_loss = 0
     model.train()
+
     for batch_data in train_loader:
         mel_spectrogram, _ = batch_data
         mel_spectrogram = mel_spectrogram.to(device)
+        # Remove channel dimension
+        mel_spectrogram = mel_spectrogram.squeeze(1)
         
         # Split the mel spectrogram into 5-frame snippets
         snippets = [mel_spectrogram[:, :, i:i+5] for i in range(mel_spectrogram.size(2) - 5 + 1)]
@@ -83,7 +97,7 @@ for epoch in tqdm(range(epochs)):
         batch_loss = 0
         for snippet in snippets:
             # Flatten snippet
-            snippet = snippet.view(snippet.shape[0], 128 * 5)
+            snippet = snippet.reshape(snippet.shape[0], -1)
             mu, logvar = model(snippet)
             loss = nll_gaussian(snippet, mu, logvar)
             batch_loss += loss
@@ -92,4 +106,87 @@ for epoch in tqdm(range(epochs)):
         optimizer.step()
         total_loss += batch_loss.item()
 
-    print(f"Epoch {epoch + 1}/{epochs}, Loss: {total_loss / len(train_loader)}")
+    # Validate current model
+    val_loss = 0
+    model.eval()
+
+    with torch.no_grad():
+        for batch_data in validation_loader:
+            mel_spectrogram, _ = batch_data
+            mel_spectrogram = mel_spectrogram.to(device)
+            # Remove channel dimension
+            mel_spectrogram = mel_spectrogram.squeeze(1)
+
+            
+            # Split the mel spectrogram into 5-frame snippets
+            snippets = [mel_spectrogram[:, :, i:i+5] for i in range(mel_spectrogram.size(2) - 5 + 1)]
+            
+            batch_val_loss = 0
+            for snippet in snippets:
+                snippet = snippet.reshape(snippet.shape[0], -1)
+                mu, logvar = model(snippet)
+                loss = nll_gaussian(snippet, mu, logvar)
+                batch_val_loss += loss
+                
+            val_loss += batch_val_loss.item()
+        
+    val_loss /= len(validation_loader)
+    
+    # Check for early stopping
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(model.state_dict(), 'best_model.pt')
+        wait = 0
+    else:
+        wait += 1
+
+    if wait > patience:
+        print("Early stopping!")
+        break
+
+    print(f"Epoch {epoch + 1}/{epochs}, Loss: {total_loss / len(train_loader)}, Val Loss: {val_loss}")
+
+################
+## EVALUATION ##
+################
+
+model.load_state_dict(torch.load('best_model.pt'))
+model.eval()
+
+test_scores = []
+test_labels = []
+
+with torch.no_grad():
+    for batch_data in test_loader:
+        mel_spectrogram, labels = batch_data
+        mel_spectrogram = mel_spectrogram.to(device)
+        # Remove channel dimension
+        mel_spectrogram = mel_spectrogram.squeeze(1)
+
+        # Split the mel spectrogram into 5-frame snippets
+        snippets = [mel_spectrogram[:, :, i:i+5] for i in range(mel_spectrogram.size(2) - 5 + 1)]
+        
+        batch_test_loss = torch.zeros([mel_spectrogram.shape[0]])
+        for snippet in snippets:
+            snippet = snippet.reshape(snippet.shape[0], -1)
+            mu, logvar = model(snippet)
+            loss = nll_gaussian(snippet, mu, logvar, eval=True)
+            
+            batch_test_loss += loss
+
+        test_scores.extend(batch_test_loss)
+        test_labels.extend(labels)
+
+# Compute AUC score
+auc_score = roc_auc_score(test_labels, test_scores)
+print(f"AUC Score: {auc_score:.4f}")
+
+# Plot ROC curve
+fpr, tpr, _ = roc_curve(test_labels, test_scores)
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, label=f"AUC = {auc_score:.4f}")
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic Curve')
+plt.legend(loc='lower right')
+plt.show()

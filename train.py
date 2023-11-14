0d7a4a20386b66b3db65caed3db6f1aab72bd23f
Lukas <selchlukas@icloud.com>
2023-11-14 07:03:00 +0000

    Utility functions in one file

---
+++ train.py
@@ -0,0 +1,84 @@
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from model import GMADE, initialize_weights
+import utils
+
+if __name__ == "__main__":
+    # Set seeds for reproducibility
+    utils.set_seeds()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate")
+    parser.add_argument("--bs", type=int, default=64, help="Batch size for dataloaders")
+    parser.add_argument("--masks", type=int, default=5, help="Number of masks for order agnostic training")
+    parser.add_argument("--hs", type=str, default="128,32,128", help="Architecture of hidden layers")
+    parser.add_argument("--epochs", type=int, default=300, help="Number of epochs to train for")
+    parser.add_argument("--device", default=torch.device('mps') if torch.backends.mps.is_available() else (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')), help="Device to run training on")
+    parser.add_argument("--samples", type=int, default=5, help="Number of hidden layer resamples")
+    parser.add_argument("--resample_every", type=int, default=20, help="How often to resample during training")
+    parser.add_argument("--patience", type=int, default=10, help="Patience for early stopping")
+    parser.add_argument("--dataset", type=str, default="dataset.pt", help="Path to preprocessed dataset")
+    args = parser.parse_args()
+
+    args.hs = [int(i) for i in args.hs.replace(" ", "").split(",")]
+    # Split and load data
+    train_set, val_set, test_set = utils.split_data(args.dataset)
+
+    train_loader = DataLoader(train_set, batch_size=args.bs)
+    val_loader = DataLoader(val_set, batch_size=args.bs)
+    test_loader = DataLoader(test_set, batch_size=args.bs)
+
+    # Initialize the model and optimizer
+    model = GMADE(128 * 5, args.hs, 128 * 5 * 2, num_masks=args.masks).to(args.device)
+    initialize_weights(model)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    # Initialize loss
+    criterion = nn.GaussianNLLLoss()
+
+    # Initialize the best validation loss and patience counter for early stopping
+    best_val_loss = float('inf')
+    waiting = 0
+
+    # Initialize tensorboard
+    writer = SummaryWriter()
+
+    # Train the model
+    for epoch in tqdm(range(args.epochs)):
+        train_loss = utils.train_epoch(model, optimizer, criterion, train_loader, args)
+        val_loss = utils.eval_epoch(model, optimizer, criterion, val_loader, args)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save({
+                'state_dict': model.state_dict(),
+                'args': args
+            }, os.path.join(writer.log_dir, "model.pt"))
+            waiting = 0
+        else:
+            waiting += 1
+
+        if waiting > args.patience:
+            break
+
+        writer.add_scalar("Loss/Train", train_loss, global_step=epoch)
+        writer.add_scalar("Loss/Val", val_loss, global_step=epoch)
+
+    # Evaluation model generalization using test set
+    state_dict = torch.load(os.path.join(writer.log_dir, "model.pt"))['state_dict']
+    roc_auc, balacc, tpr, tnr = utils.test_model(model, test_loader, state_dict, args)
+
+    writer.add_scalar("Metrics/ROCAUC", roc_auc)
+    writer.add_scalar("Metrics/BALACC", balacc)
+    writer.add_scalar("Metrics/TPR", tpr)
+    writer.add_scalar("Metrics/TNR", tnr)
+
+    writer.close()
--- utils.py
+++ utils.py
@@ -1,24 +1,13 @@
-import argparse
-import os
 import random
 import torch
 import torch.nn as nn
 import torch.nn.parallel
-import torch.optim as optim
-from torch.utils.data import DataLoader, random_split, Subset, Dataset
-from torch.utils.tensorboard import SummaryWriter
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-import torchvision.utils as vutils
+from torch.utils.data import Dataset
+
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from tqdm import tqdm
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
-
-from model import GMADE, initialize_weights
 
 class RespiratorySoundDataset(Dataset):
     def __init__(self, X, y):
@@ -33,12 +22,12 @@ class RespiratorySoundDataset(Dataset):
         return self.X[idx], self.y[idx]
 
 
-def set_seeds(seed=42):
+def set_seeds(seed=999):
     np.random.seed(seed)
     random.seed(seed)
     torch.manual_seed(seed)
 
-def split_data(dataset, prevent_leakage=False, seed=42):
+def split_data(dataset, prevent_leakage=False, seed=999):
     data = torch.load(dataset)
     
     recording_ids = data['recording_ids']
@@ -47,7 +36,7 @@ def split_data(dataset, prevent_leakage=False, seed=42):
 
     if prevent_leakage:
         unique_recording_ids = torch.unique(recording_ids)
-        train_ids, test_ids = train_test_split(unique_recording_ids, test_size=0.2, random_state=seed)
+        train_ids, test_ids = train_test_split(unique_recording_ids, test_size=0.2, random_state=seed, stratify=labels)
 
         # Create masks for selecting data
         train_mask = torch.isin(recording_ids, train_ids)
@@ -58,10 +47,10 @@ def split_data(dataset, prevent_leakage=False, seed=42):
         X_test, y_test = cycles[test_mask], labels[test_mask]
     else:
         # Random splitting 80/20
-        X_train, X_test, y_train, y_test = train_test_split(cycles, labels, test_size=0.2, random_state=seed)
+        X_train, X_test, y_train, y_test = train_test_split(cycles, labels, test_size=0.2, random_state=seed, stratify=labels)
 
     # Further splitting of train set into validation
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=seed)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=seed, stratify=y_train)
 
     X_train, y_train = X_train[y_train == 0], y_train[y_train == 0]
 
@@ -70,6 +59,17 @@ def split_data(dataset, prevent_leakage=False, seed=42):
     test_set = RespiratorySoundDataset(X_test, y_test)
 
     return train_set, val_set, test_set
+
+def get_optimal_threshold(losses, labels):
+    accuracies = []
+    for threshold in losses:
+        y_pred = losses > threshold
+        tpr = np.sum((y_pred == 1) & (labels == 1)) / np.sum(labels == 1)
+        tnr = np.sum((y_pred == 0) & (labels == 0)) / np.sum(labels == 0)
+        accuracy = 0.5 * (tpr + tnr)
+        accuracies.append(accuracy)
+    optimal_threshold = losses[np.argmax(accuracies)]
+    return optimal_threshold
 
 def train_epoch(model, optimizer, criterion, dataloader, args):
     train_loss = 0
@@ -112,6 +112,8 @@ def train_epoch(model, optimizer, criterion, dataloader, args):
     return train_loss
 
 def eval_epoch(model, optimizer, criterion, dataloader, args):
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=args.patience // 2, min_lr=3e-5, factor=0.1)
+
     val_loss = 0
     model.eval()
 
@@ -145,6 +147,7 @@ def eval_epoch(model, optimizer, criterion, dataloader, args):
         val_loss += loss.item()
 
     val_loss = val_loss / len(dataloader)
+    scheduler.step(val_loss)
     return val_loss
 
 def test_model(model, dataloader, state_dict, args):
@@ -183,7 +186,7 @@ def test_model(model, dataloader, state_dict, args):
                 # Reshape snippets back to original shape
                 snippet = snippet.view(-1, 128, 5)
 
-                loss = criterion(mu, snippet, logvar.exp()).mean((1, 2))
+                loss = criterion(mu, snippet, logvar.exp()).sum((1, 2))
                 batch_loss += loss.cpu()
 
             batch_loss /= len(snippets)
@@ -194,73 +197,17 @@ def test_model(model, dataloader, state_dict, args):
     scores = np.array(scores)
     labels = np.array(labels)
 
-    return roc_auc_score(labels, scores)
+    threshold = get_optimal_threshold(scores, labels)
+    predictions = (scores > threshold)
 
-if __name__ == "__main__":
-    # Set seeds for reproducibility
-    set_seeds()
+    roc_auc = roc_auc_score(labels, scores)
+    tpr = np.sum((predictions == 1) & (labels == 1)) / np.sum(labels == 1)
+    tnr = np.sum((predictions == 0) & (labels == 0)) / np.sum(labels == 0)
+    balanced_accuracy = 0.5 * (tpr + tnr)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate")
-    parser.add_argument("--bs", type=int, default=64, help="Batch size for dataloaders")
-    parser.add_argument("--masks", type=int, default=5, help="Number of masks for order agnostic training")
-    parser.add_argument("--hs", type=str, default="128,32,128", help="Architecture of hidden layers")
-    parser.add_argument("--epochs", type=int, default=300, help="Number of epochs to train for")
-    parser.add_argument("--device", default=torch.device('mps') if torch.backends.mps.is_available() else (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')), help="Device to run training on")
-    parser.add_argument("--samples", type=int, default=5, help="Number of hidden layer resamples")
-    parser.add_argument("--resample_every", type=int, default=20, help="How often to resample during training")
-    parser.add_argument("--patience", type=int, default=10, help="Patience for early stopping")
-    parser.add_argument("--dataset", type=str, default="dataset.pt", help="Path to preprocessed dataset")
-    args = parser.parse_args()
+    print("ROC-AUC Score: ", roc_auc.round(2))
+    print("BALACC: ", balanced_accuracy.round(2))
+    print("TPR: ", tpr.round(2))
+    print("TNR: ", tnr.round(2))
 
-    args.hs = [int(i) for i in args.hs.replace(" ", "").split(",")]
-    # Split and load data
-    train_set, val_set, test_set = split_data(args.dataset, 999)
-
-    train_loader = DataLoader(train_set, batch_size=args.bs)
-    val_loader = DataLoader(val_set, batch_size=args.bs)
-    test_loader = DataLoader(test_set, batch_size=args.bs)
-
-    # Initialize the model and optimizer
-    model = GMADE(128 * 5, args.hs, 128 * 5 * 2, num_masks=args.masks).to(args.device)
-    # initialize_weights(model)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
-    # Initialize loss
-    criterion = nn.GaussianNLLLoss()
-
-    # Initialize the best validation loss and patience counter for early stopping
-    best_val_loss = float('inf')
-    waiting = 0
-
-    # Initialize tensorboard
-    writer = SummaryWriter()
-
-    # Train the model
-    for epoch in tqdm(range(args.epochs)):
-        train_loss = train_epoch(model, optimizer, criterion, train_loader, args)
-        val_loss = eval_epoch(model, optimizer, criterion, val_loader, args)
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            torch.save({
-                'state_dict': model.state_dict(),
-                'args': args
-            }, os.path.join(writer.log_dir, "model.pt"))
-            waiting = 0
-        else:
-            waiting += 1
-
-        if waiting > args.patience:
-            break
-
-        writer.add_scalar("Loss/Train", train_loss, global_step=epoch)
-        writer.add_scalar("Loss/Val", val_loss, global_step=epoch)
-
-    # Evaluation model generalization using test set
-    state_dict = torch.load(os.path.join(writer.log_dir, "model.pt"))['state_dict']
-    roc_auc_score = test_model(model, test_loader, state_dict, args)
-    print("ROC-AUC Score: ", roc_auc_score)
-    writer.add_scalar("Metrics/ROCAUC", roc_auc_score)
-
-    writer.close()
+    return roc_auc, balanced_accuracy, tpr, tnr
